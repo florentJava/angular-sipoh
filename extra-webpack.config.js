05f9221ef7df7781f95aa981ad7f0d1fbf240693
florentJava <njetejie.florent21@myiuc.com>
2024-09-09 13:14:58 +0100

    page a-propos ok

--- extra-webpack.config.js
+++
@@ -1,9 +0,0 @@
-// extra-webpack.config.js
-module.exports = {
-    externals: {
-        'assets/contactBG.png': 'commonjs2 assets/contactBG.png',
-        'assets/nousBg.png': 'commonjs2 assets/nousBg.png',
-        'assets/contactBG.png': 'commonjs2 assets/bg_1.png'
-    }
-  };
-  
