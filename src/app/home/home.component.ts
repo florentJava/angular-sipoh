3a38a33f00a9b74abe65355a16f6c93f821e2f53
florentJava <njetejie.florent21@myiuc.com>
2024-09-20 13:21:37 +0100

    menu ameliorer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import * as AOS from 'aos'; //AOS - 1
 import { FooterComponent } from '../ui/footer/footer.component';
 import { Section1Component } from '../ui/home/section1/section1.component';
 import { Section2Component } from '../ui/home/section2/section2.component';
 import { Section3Component } from '../ui/home/section3/section3.component';
 import { NavbarComponent } from '../ui/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-home',
@@ -17,5 +19,11 @@ import { NavbarComponent } from '../ui/navbar/navbar.component';
 })
 export class HomeComponent {
   show = true;
+
+
+  ngOnInit() {
+    AOS.init({once: true});//AOS - 2
+    AOS.refresh();//refresh method is called on window resize and so on, as it doesn't require to build new store with AOS elements and should be as light as possible.
+  }
 
 }
